1f8e7ced83eba774e63d534fb73376bac9e7d2b2
jacky <jacky@taoqi001.com>
2024-02-02 00:43:09 +0800

    nil

--- say.go
+++ say.go
@@ -1,23 +1,8 @@
 package gomodone
 
-import (
-    "errors"
-    "fmt"
-)
+import "fmt" 
 
-// Hi returns a friendly greeting
-// Hi returns a friendly greeting in language lang
-func SayHi(name, lang string) (string, error) {
-    switch lang {
-    case "en":
-        return fmt.Sprintf("Hi, %s!", name), nil
-    case "pt":
-        return fmt.Sprintf("Oi, %s!", name), nil
-    case "es":
-        return fmt.Sprintf("¡Hola, %s!", name), nil
-    case "fr":
-        return fmt.Sprintf("Bonjour, %s!", name), nil
-    default:
-        return "", errors.New("unknown language")
-    }
+// say Hi to someone
+func SayHi(name string) string {
+   return fmt.Sprintf("Hi, %s!!", name)
 }
